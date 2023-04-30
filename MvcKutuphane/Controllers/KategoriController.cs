cf5f60db33693467696693dd8495ece343d0e261
Batuhan Kılıç <batuhan.bkl10@gmail.com>
2023-04-30 14:37:27 +0300

    initial project

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    
+    public class KategoriController : Controller
+    {
+        // GET: KasaTipi
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        public ActionResult Index()
+        {
+            var değerler = db.TBLKATEGORI.ToList();
+            return View(değerler);
+        }
+
+        [HttpGet]
+        public ActionResult Kategoriekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORI.Find(id);
+
+            return View("KategoriGetir", ktgr);
+
+        }
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD = p.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
